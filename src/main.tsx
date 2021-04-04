67011ee1efbf12c95285939dade82f67aadb3135
krypticallusion <krypticallusion@gmail.com>
2021-04-04 12:20:24 +0530

    Init
    Signed-off-by: krypticallusion <krypticallusion@gmail.com>

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import {registerSW} from "virtual:pwa-register"
+
+registerSW({
+  onOfflineReady() {
+    
+  }
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import {VitePWA} from "vite-plugin-pwa"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh(), VitePWA({
+    manifest: {
+      name: "TEST"
+    }    
+  })]
+})
